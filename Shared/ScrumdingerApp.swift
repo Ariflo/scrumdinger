52c8b3d61c8bc9d9456a379e61dd013bdaf59f90
ari@addressable.app <ari@addressable.app>
2021-03-30 16:45:21 -0700

    Initial Commit - Lessons 1 + 2

---
+++ Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by Ari on 3/30/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
